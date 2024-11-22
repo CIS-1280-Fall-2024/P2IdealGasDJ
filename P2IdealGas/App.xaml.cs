c62b4c519a060c486ef53879591e84f3208ae551
jaram <jaram@MSJARAMILLO>
2024-11-22 09:07:41 -0700

    Add project files.

---
+++ P2IdealGas/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace P2IdealGas
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ P2IdealGas/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace P2IdealGas
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
